15e00c2b4c52375ee702ac76fe63edc294002205
adonis-agelis <agelisadonis@gmail.com>
2023-08-27 19:09:10 +0300

    create bottombat for mobile view

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+
 export default function BottomBar() {
-  return <h1>Bottom</h1>;
+  const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map(link => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
