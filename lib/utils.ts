6c10489ac18e29d78fb74ab269f14a9628a4b660
adonis-agelis <agelisadonis@gmail.com>
2023-08-28 22:45:03 +0300

    use uploadthing to upload images

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,13 +6,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// created by chatgpt
 export function isBase64Image(imageData: string) {
   const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
+
   return base64Regex.test(imageData);
 }
 
-// created by chatgpt
 export function formatDateString(dateString: string) {
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
@@ -31,13 +30,13 @@ export function formatDateString(dateString: string) {
   return `${time} - ${formattedDate}`;
 }
 
-// created by chatgpt
 export function formatThreadCount(count: number): string {
   if (count === 0) {
     return "No Threads";
   } else {
     const threadCount = count.toString().padStart(2, "0");
     const threadWord = count === 1 ? "Thread" : "Threads";
+
     return `${threadCount} ${threadWord}`;
   }
 }
