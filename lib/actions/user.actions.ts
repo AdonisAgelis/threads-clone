b90b48338d45e3c523406a81f5f8ca45849be185
adonis-agelis <agelisadonis@gmail.com>
2023-08-30 21:22:25 +0300

    mongoose file to connect to DB

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDatabase } from "../mongoose";
+
+export async function updateUser(
+  userId: string,
+  username: string,
+  name: string,
+  bio: string,
+  image: string,
+  path: string
+): Promise<void> {
+  connectToDatabase();
+
+  try {
+    await User.findOneAndUpdate(
+      {
+        id: userId,
+      },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      {
+        upsert: true,
+      }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error) {
+    throw new Error(`Failed to create/update user: ${error}`);
+  }
+}
