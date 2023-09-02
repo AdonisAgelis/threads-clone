8955624cece6997b481ca5610e6ba29f85f79c3c
adonis-agelis <agelisadonis@gmail.com>
2023-09-03 02:03:09 +0300

    create-thread page

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,21 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+import PostThread from "@/components/forms/PostThread";
+
+export default async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  return (
+    <>
+      <h1 className="head-text">Create a thread</h1>
+      <PostThread userId={userInfo._id} />
+    </>
+  );
+}
