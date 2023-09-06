4072f2ce1ef6666892db68f47351ac485e7aa022
adonis-agelis <agelisadonis@gmail.com>
2023-09-06 23:54:37 +0300

    completed onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,16 @@
 import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-async function Page() {
+export default async function Page() {
   const user = await currentUser();
 
-  const userInfo = {};
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
     id: user?.id,
@@ -27,5 +33,3 @@ async function Page() {
     </main>
   );
 }
-
-export default Page;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default async function Home() {
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
