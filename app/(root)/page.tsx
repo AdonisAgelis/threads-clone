351536c9838bd688a9d2c758b3b5d47e8f4c2cac
adonis-agelis <agelisadonis@gmail.com>
2023-09-07 23:59:50 +0300

    render a thread

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,45 @@
+import { fetchThreads } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
+import ThreadCard from "@/components/cards/ThreadCard";
+
 export default async function Home() {
+  const { threads, nextPage } = await fetchThreads(1, 30);
+  const user = await currentUser();
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {!threads?.length ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {threads.map(
+              ({
+                _id,
+                text,
+                author,
+                parentId,
+                community,
+                createdAt,
+                children,
+              }) => (
+                <ThreadCard
+                  key={_id}
+                  threadId={_id}
+                  currentUserId={user?.id || ""}
+                  text={text}
+                  author={author}
+                  parentId={parentId}
+                  community={community}
+                  createdAt={createdAt}
+                  comments={children}
+                />
+              )
+            )}
+          </>
+        )}
+      </section>
     </>
   );
 }
