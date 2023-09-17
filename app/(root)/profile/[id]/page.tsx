2433d8b7a512e39166bc19c2b3af98dd96d1927e
adonis-agelis <agelisadonis@gmail.com>
2023-09-17 19:29:40 +0300

    profile tabs implementation

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -2,6 +2,9 @@ import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { fetchUser } from "@/lib/actions/user.actions";
 import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
@@ -22,6 +25,29 @@ export default async function Page({ params }: { params: { id: string } }) {
         imgUrl={userInfo.image}
         bio={userInfo.bio}
       />
+      <div className="mt-9">
+        <Tabs className="w-full" defaultValue="threads">
+          <TabsList className="tab">
+            {profileTabs.map(tab => (
+              <TabsTrigger className="tab" key={tab.label} value={tab.value}>
+                <Image
+                  className="object-contain"
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                />
+                <p className="max-sm:hidden">{tab.label}</p>
+                {tab.label === "Threads" && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo?.threads?.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+      </div>
     </section>
   );
 }
