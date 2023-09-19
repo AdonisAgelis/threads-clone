f814be75cf18d9a6a9aea54e6e7646a7338ed99f
adonis-agelis <agelisadonis@gmail.com>
2023-09-19 23:00:17 +0300

    fetch user threads action for profile

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDatabase } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface Params {
   userId: string;
@@ -61,5 +62,30 @@ export async function fetchUser(userId: string) {
     // })
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
+
+export async function fetchUserThreads(userId: string) {
+  try {
+    connectToDatabase();
+
+    // TODO - Populate community
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user threads: ${error.message}`);
   }
 }
