bdbe24fdf67559772ef446fde2fd94001efc8f33
adonis-agelis <agelisadonis@gmail.com>
2023-09-25 22:13:41 +0300

    fetch all users action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDatabase } from "../mongoose";
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder, _FilterQuery } from "mongoose";
 
 interface Params {
   userId: string;
@@ -87,5 +88,57 @@ export async function fetchUserThreads(userId: string) {
     return threads;
   } catch (error: any) {
     throw new Error(`Failed to fetch user threads: ${error.message}`);
+  }
+}
+
+export async function fetchAllUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDatabase();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const regex = new RegExp(searchString, "i");
+
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId },
+    };
+
+    if (searchString.trim() !== "") {
+      query["$or"] = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    const nextPage = totalUsersCount > skipAmount + users.length;
+
+    return {
+      users,
+      nextPage,
+    };
+  } catch (error: any) {
+    throw new Error(`Failed to fetch all users: ${error.message}`);
   }
 }
