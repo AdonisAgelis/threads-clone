e4666621d21cc9a3b1316a52adb8877556ad1a76
adonis-agelis <agelisadonis@gmail.com>
2023-09-25 21:18:19 +0300

    added styles to auth pages

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <section className="auth auth-bg bg-cyan-600">
+      <SignUp />
+    </section>
+  );
 }
