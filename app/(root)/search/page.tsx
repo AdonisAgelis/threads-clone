41f3d6b8cef041a652d5e63665930d096e3382f0
adonis-agelis <agelisadonis@gmail.com>
2023-09-25 22:14:06 +0300

    search page

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,7 +1,47 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchAllUsers, fetchUser } from "@/lib/actions/user.actions";
+import UserCard from "@/components/cards/UserCard";
+
 export default async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  // Fetch all users
+  const result = await fetchAllUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
   return (
     <section>
       <h1 className="head-text mt-10">Search</h1>
+      {/* Search bar */}
+      <div className="mt-14 flex flex-col gap-9">
+        {!result.users.length ? (
+          <p className="no-result">No users found</p>
+        ) : (
+          <>
+            {result.users.map(user => (
+              <UserCard
+                key={user.id}
+                id={user.id}
+                name={user.name}
+                username={user.username}
+                imgUrl={user.image}
+                userType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
 }
