dfef647e0fdcdfb99bee2fa72daa73021dcd2c2c
adonis-agelis <agelisadonis@gmail.com>
2023-09-28 22:26:51 +0300

    activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,7 +1,50 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import Link from "next/link";
+import Image from "next/image";
+
 export default async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const activity = await getActivity(userInfo._id);
+
   return (
     <section>
       <h1 className="head-text mt-10">Activity</h1>
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map(activity => (
+              <Link key={activity.id} href={`/thread/${activity.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    className="rounded-full object-cover"
+                    src={activity.author.image}
+                    alt="Profile Picture"
+                    width={20}
+                    height={20}
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet</p>
+        )}
+      </section>
     </section>
   );
 }
